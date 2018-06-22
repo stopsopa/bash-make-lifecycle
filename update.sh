8c8275733ed4d005aa83cac3a9821fc47b0f8f84
Szymon Dzialowski <sd@phase-ii.com>
2018-06-22 14:32:32 +0100

    test

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-set -o xtrace
+# set -o xtrace
 
 trim() {
     local var="$*"
@@ -12,6 +12,17 @@ trim() {
     var="${var%"${var##*[![:space:]]}"}"
     echo -n "$var"
 }
+
+DIFF="$(git diff --numstat)"
+
+DIFF="$(trim "$DIFF")"
+
+if [ "$DIFF" != "" ]; then
+
+    printf "\n\n    Error: First commit changes ...\n\n";
+
+    exit 1
+fi
 
 DIFF="$(git diff --numstat master origin/master)"
 
