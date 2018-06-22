30e37ef661a96a926c463c6d6b81292e241ed6e1
Szymon Dzialowski <sd@phase-ii.com>
2018-06-22 14:42:20 +0100

    final update

--- update.sh
+++ update.sh
@@ -4,6 +4,10 @@
 
 # set -o xtrace
 
+ORIGIN="origin"
+LOCALBRANCH="master"
+REMOTEBRANCH="master"
+
 trim() {
     local var="$*"
     # remove leading whitespace characters
@@ -13,18 +17,35 @@ trim() {
     echo -n "$var"
 }
 
+function red {
+    printf "\e[31m$1\e[0m\n"
+}
+
+function green {
+    printf "\e[32m$1\e[0m\n"
+}
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != $LOCALBRANCH ]; then
+
+    red "switch first branch to <$LOCALBRANCH>"
+
+    exit 1
+fi
+
+green "\ncurrent branch: $LOCALBRANCH";
+
 DIFF="$(git diff --numstat)"
 
 DIFF="$(trim "$DIFF")"
 
 if [ "$DIFF" != "" ]; then
 
-    printf "\n\n    Error: First commit changes ...\n\n";
+    red "\n\n    Error: First commit changes ...\n\n";
 
     exit 1
 fi
 
-DIFF="$(git diff --numstat master origin/master)"
+DIFF="$(git diff --numstat $LOCALBRANCH $ORIGIN/$REMOTEBRANCH)"
 
 DIFF="$(trim "$DIFF")"
 
@@ -32,16 +53,13 @@ if [ "$DIFF" != "" ]; then
 
     npm version patch
 
-echo 'stop...'
-
-    exit 1
-
-    git push origin master
+    git push $ORIGIN $REMOTEBRANCH
 
     npm publish
 
 else
-    printf "\n\n    Nothing new to publish\n\n";
+
+    red "\n\n    Nothing new to publish\n\n";
 fi
 
 
